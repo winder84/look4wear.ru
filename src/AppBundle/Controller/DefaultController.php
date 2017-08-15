77c9a2bd4ba34a0af37b9bccc46b23b17bc2832f
winder <winder84@mail.ru>
2017-08-16 00:57:39 +0300

    search & templates

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,12 +2,23 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    /**
+     * @var EntityManager
+     */
+    protected static $em;
+
+    /**
+     * @var int
+     */
+    protected static $resultsOnPage = 30;
+
     /**
      * @Route("/", name="homepage")
      */
@@ -20,18 +31,40 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/search/{search}", name="search_page")
+     * @Route("/search/{search}/{page}", name="search_page")
+     * @param $search
+     * @param $page
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function searchAction(Request $request, $search)
+    public function searchAction($search, $page = 0)
     {
+        $matches = [];
+        self::$em = $this->getDoctrine()->getManager();
+        self::$em->getConnection()->getConfiguration()->setSQLLogger(null);
         $sphinxSearch = $this->get('iakumai.sphinxsearch.search');
-        $sphinxSearch->setLimits(0, 100);
-        $goods = $sphinxSearch->search($search, ['goods']);
-        echo '<pre>';
-        var_dump($goods['matches']);
-        echo '</pre>';
-        return $this->render('AppBundle:look4wear:index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        $sphinxSearch->setLimits($page * self::$resultsOnPage, self::$resultsOnPage);
+        $searchGoods = $sphinxSearch->search($search, ['goods']);
+        if (isset($searchGoods['matches'])) {
+            $matches = $searchGoods['matches'];
+        }
+        $totalCount = $searchGoods['total_found'];
+        $goodsIds = [];
+        foreach ($matches as $matchesKey => $matchesItem) {
+            $goodsIds[] = $matchesKey;
+        }
+        $qb = self::$em->createQueryBuilder();
+        $qb->select('Goods')
+            ->from('AppBundle:Goods', 'Goods')
+            ->where('Goods.id IN (:goodsIds)')
+            ->andWhere('Goods.isDelete = 0')
+            ->setParameter('goodsIds', $goodsIds);
+        $query = $qb->getQuery();
+        $goods = $query->getResult();
+
+        var_dump($totalCount);
+        return $this->render('AppBundle:look4wear:index.html.twig', [
+            'goods' => $goods,
+            'totalCount' => $totalCount,
+        ]);
     }
 }
