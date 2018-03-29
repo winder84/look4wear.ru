1d7119759d8406446990212f3d4fb016c35876cb
winder <winder84@mail.ru>
2018-03-30 02:17:44 +0300

    new styles

---
+++ src/AppBundle/Resources/public/js/main.js
@@ -0,0 +1,21 @@
+var bLazy = new Blazy({
+    breakpoints: [{
+        width: 420 // Max-width
+        , src: 'data-src-small'
+    }]
+    , success: function(element){
+        setTimeout(function(){
+            // We want to remove the loader gif now.
+            // First we find the parent container
+            // then we remove the "loading" class which holds the loader image
+            var parent = element.parentNode;
+            parent.className = parent.className.replace(/\bloading\b/,'');
+        }, 200);
+    }
+});
+
+$('.productLink').hover(function () {
+    $(this).prop('href', '/goods/buy/' + $(this).data('productAlias'));
+}, function () {
+    $(this).prop('href', '#');
+});
