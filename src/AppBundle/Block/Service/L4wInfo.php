7c9da128a1425aa8cdb0d6ffac072757fdf98c44
rustam <rustam@nodasoft.com>
2018-03-29 10:37:52 +0300

    update l4w info

--- src/AppBundle/Block/Service/L4wInfo.php
+++ src/AppBundle/Block/Service/L4wInfo.php
@@ -1,6 +1,8 @@
 <?php
 namespace AppBundle\Block\Service;
 
+use Doctrine\ORM\EntityManager;
+use IAkumaI\SphinxsearchBundle\Search\Sphinxsearch;
 use Sonata\BlockBundle\Block\BlockContextInterface;
 use Sonata\BlockBundle\Block\Service\AbstractBlockService;
 use Sonata\BlockBundle\Block\Service\BlockServiceInterface;
@@ -11,24 +13,56 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 class L4wInfo extends AbstractBlockService implements BlockServiceInterface
 {
 
+    /**
+     * @var EntityManager
+     */
+    protected static $em;
+
+    /**
+     * @var Sphinxsearch
+     */
+    protected static $sphinxSearch;
+
+    public function __construct(EntityManager $entityManager, Sphinxsearch $sphinxSearch)
+    {
+        parent::__construct(null, '@templating');
+        self::$em = $entityManager;
+        self::$sphinxSearch = $sphinxSearch;
+        self::$em->getConnection()->getConfiguration()->setSQLLogger(null);
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
     public function configureOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults([
             'title' => 'L4wInfo',
-            'template' => 'AppBundle:look4wear:l4w.info.block.html.twig',
+            'template' => 'AppBundle:Block:l4w.info.block.html.twig',
         ]);
     }
 
+    /**
+     * @return string
+     */
     public function getName()
     {
         return "L4wInfo";
     }
 
+    /**
+     * @param BlockInterface $block
+     * @return array
+     */
     public function getCacheKeys(BlockInterface $block)
     {
         return [];
     }
 
+    /**
+     * @param $media
+     * @return array
+     */
     public function getJavascripts($media)
     {
         return [];
@@ -39,17 +73,77 @@ class L4wInfo extends AbstractBlockService implements BlockServiceInterface
         return [];
     }
 
+    /**
+     * @param BlockContextInterface $blockContext
+     * @param Response|null $response
+     * @return Response
+     */
     public function execute(BlockContextInterface $blockContext, Response $response = null)
     {
+        $lessGoodsArray = [];
+        $categories = self::$em
+            ->getRepository('AppBundle:Category')
+            ->findAll();
 
-        return $this->renderResponse('AppBundle:look4wear:l4w.info.block.html.twig', [
+        foreach ($categories as $category) {
+            $lessGoodsArray[$category->getId()] = $this->getTopCategoryVendorsCounts($category);
+        }
+
+        return $this->renderResponse('AppBundle:Block:l4w.info.block.html.twig', [
             'block' => $blockContext->getBlock(),
             'settings' => $blockContext->getSettings(),
             'info' => [
                 'title' => 'Заголовок',
-                'content' => 'Контент',
+                'lessGoodsArray' => $lessGoodsArray,
             ],
         ], $response);
+    }
+
+    /**
+     * @param $category
+     * @return array
+     */
+    public function getTopCategoryVendorsCounts($category)
+    {
+        if ($category) {
+            $lessGoodsVendors = [];
+            $excludeWords = explode(';', $category->getExcludeWords());
+            $searchString = $category->getSearchString();
+            if (array_filter($excludeWords)) {
+                $searchString .= ' -' . implode(' -', $excludeWords);
+            }
+            $categoryData = $category->getData();
+            if (isset($categoryData['topVendors'])) {
+                $topVendors = $categoryData['topVendors'];
+                foreach ($topVendors as $topVendorName => $topVendorCount) {
+                    $searchString .= ' ' . $topVendorName;
+                    $searchGoods = $this->searchByStringAndLimit($searchString, 1);
+                    $totalCount = $searchGoods['total_found'];
+                    if ($totalCount < 10) {
+                        $lessGoodsVendors = [
+                            'category' => $category,
+                            'topVendorName' => $topVendorName,
+                            'topVendorCount' => $topVendorCount,
+                            'realCount' => $totalCount,
+                        ];
+                    }
+                }
+            }
+            return $lessGoodsVendors;
+        }
+
+    }
+
+    /**
+     * @param $searchString
+     * @param $limit
+     * @return mixed
+     */
+    public function searchByStringAndLimit($searchString, $limit)
+    {
+        self::$sphinxSearch->setLimits(0, $limit, 100000);
+        self::$sphinxSearch->SetMatchMode(SPH_MATCH_EXTENDED);
+        return self::$sphinxSearch->query($searchString);
     }
 }
 
