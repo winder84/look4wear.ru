0cf4cef3126fb5e36e82423085eb6af9520f21c8
winder <winder84@mail.ru>
2018-04-09 00:52:49 +0300

    new 404

---
+++ src/AppBundle/Controller/ExceptionController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Category;
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ExceptionController extends Controller
+{
+
+    /**
+     * @var EntityManager
+     */
+    protected static $em;
+
+    /**
+     * @var array
+     */
+    protected static $mainMenuCategories = [];
+
+    /**
+     * @Route("/404")
+     */
+    public function show404Action()
+    {
+        self::$em = $this->getDoctrine()->getManager();
+        $mainMenuCategories = self::$em
+            ->getRepository('AppBundle:Category')
+            ->findBy([
+                'isActive' => true,
+                'inMainMenu' => true,
+            ]);
+        foreach ($mainMenuCategories as $mainMenuCategory) {
+            self::$mainMenuCategories[] = [
+                'title' => $mainMenuCategory->getTitle(),
+                'link' => $this->getParentCategoriesUrl($mainMenuCategory) . $mainMenuCategory->getAlias(),
+            ];
+        }
+        return $this->render('AppBundle:Exception:show404.html.twig', [
+            'mainMenuCategories' => self::$mainMenuCategories
+        ]);
+    }
+
+    /**
+     * @param Category $category
+     * @return string
+     */
+    private function getParentCategoriesUrl(Category $category)
+    {
+        $parentCategories = [];
+        $parentsUrl = '/catalog/';
+        $parentCategories[] = $parentCategory = $category->getParentCategory();
+        while ($parentCategory && $parentCategories[] = $parentCategory = $parentCategory->getParentCategory()) {
+        }
+        if (!end($parentCategories)) {
+            array_pop($parentCategories);
+        }
+        $parentCategories = array_reverse($parentCategories);
+        foreach ($parentCategories as $parentCategory) {
+            $parentsUrl .= $parentCategory->getAlias() . '/';
+        }
+
+        return $parentsUrl;
+    }
+}
