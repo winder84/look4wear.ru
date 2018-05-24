ebce8974c8bdbd4341426dd18d4ee0f5f532897b
rustam <rustam@nodasoft.com>
2018-05-24 12:45:26 +0300

    styles

--- src/AppBundle/Resources/public/js/main.js
+++ src/AppBundle/Resources/public/js/main.js
@@ -23,5 +23,6 @@ $('.productLink').hover(function () {
 $(function() {
     $('.menu__icon').on('click', function() {
         $(this).closest('.mobileBlock').toggleClass('menu_state_open');
+        $('.menu__links').toggle();
     });
 });
